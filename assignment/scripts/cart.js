c1aba2824dc9e1136134376e0c3c65c4509e6a9e
Tristan Bridges <mophead4@gmail.com>
2024-04-18 13:28:14 -0500

    addItem and listItems are function, working on empty

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,33 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+
+function listItems(array){
+    for (let item of array)
+    console.log('In basket:', item);
+}
+
+function empty(array){
+    array.splice(0)
+    return array;
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding tacos (expect true)', addItem('tacos'));
+console.log(`Basket is ${basket}`);
+console.log('Adding subs', addItem('subs'));
+console.log('Adding wings', addItem('wings'));
+console.log('Basket is:', basket);
+console.log(empty(basket));
+console.log('Basket is:', basket);
+
+
 
 
 
